b21db50f7e4d316f0cac773af7d78ddee1d35d6f
lawfirm-sinsa <lawfirm.sinsa@gmail.com>
2019-11-11 09:05:15 +0900

    change on notebook

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,36 +1,15 @@
 import os
 
 from flask import Flask
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity 
 
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )    
+    app = Flask(__name__, instance_relative_config=True)        
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    #from . import db
-    #db.init_app(app)
+    app.config['JWT_SECRET_KEY'] = 'apple'  # Change this!
+    jwt = JWTManager(app)
 
     from . import index
     app.register_blueprint(index.bp)
--- flaskr/temp.py
+++ flaskr/temp.py
@@ -38,3 +38,111 @@ if post is None:
 if check_author and post['author_id'] != g.user['id']:
     abort(403)
 return post
+
+# auth.py backup / 2019. 11. 09. 
+
+import functools
+
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from functools import wraps
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+client = MongoClient('localhost',27017)
+db = client.dbLFD
+
+SECRET_KEY = 'apple'
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username_give']
+        email = request.form['email_give']
+        password = request.form['password_give']
+
+        pw_hash = generate_password_hash(password)
+        #db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not email:
+            error = 'email is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.user_test3.find_one({'username':request.form['username_give']}):
+            error = 'User {} is already registered.'.format(username)
+        elif db.user_test3.find_one({'email':request.form['email_give']}):
+            error = 'email {} is already registered.'.format(email)        
+        if error is None:
+            db.user_test3.insert_one({'username':username, 'email':email, 'password':pw_hash})                
+            #db.user_test3.insert_one({'username':username, 'email':email, 'password':generate_password_hash(password)})                
+            return jsonify({'result':'success'})
+        else:
+            return jsonify({'result':'fail'})
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        email = request.form['email_give']
+        password = request.form['password_give']        
+        error = None
+        user = db.user_test3.find_one({'email':email})            
+        print(user)
+        if user is None:            
+            return jsonify({'result':'fail', 'msg':'email is incorrect'})
+        elif not check_password_hash(user['password'], password):
+            return jsonify({'result':'fail', 'msg':'password is incorrect'})        
+        else:            
+            payload = {
+                'email' : email,
+                'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=30)
+            }
+            token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+            print(token)
+            return jsonify({'result':'success', 'msg':'로그인 성공', 'token':token})        
+    return render_template('auth/login.html')
+
+@bp.route('/validation', methods=['GET'])
+def api_valid():
+    token_receive = request.headers['token_give']
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+        userinfo = db.user_test3.find_one({'email':payload['email']},{'_id':0})         
+        print(userinfo)
+        return jsonify({'result':'success', 'userinfo':userinfo, 'username':userinfo['username']})   
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result':'fail', 'msg':'로그인 시간이 만료되었습니다'})
+
+def jwt_token_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not 'token_give' in request.headers:
+            return jsonify({
+                'msg':'token is not given'
+            }), 400
+        token_receive = request.headers['token_give']
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        except:
+            return jsonify({
+                'msg' : 'invalid token given'
+            }), 400
+        kwargs['payload'] = payload
+        return func(*args, **kwargs)        
+    return decorated_function
+
+
+@bp.route('/logout')
+def logout():
+    token = ""
+    return redirect(url_for('index.home'))
+
+
